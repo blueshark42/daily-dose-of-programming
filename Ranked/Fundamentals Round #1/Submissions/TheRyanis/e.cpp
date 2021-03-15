b4ba5ec1f73c46fe193b822a0ed695a34150dcdb
blueshark <51487866+blueshark42@users.noreply.github.com>
2021-03-15 14:24:18 +0100

    Update e.cpp

--- Ranked/Fundamentals Round #1/Submissions/TheRyanis/e.cpp
+++ Ranked/Fundamentals Round #1/Submissions/TheRyanis/e.cpp
@@ -1,36 +1,39 @@
-#include <bits/stdc++.h>
+#include<iostream>
+#include<string>
+#include<cmath>
+
 using namespace std;
 
-#define ll long long
-#define arr array
-#define inf int(1e9)
-
-void solve() {
-	int n, a[1000], ans;
-	cin>>n;
-
-	for(int i=0;i<n;++i)
-		cin>>a[i];
-
-	ans=a[0];
-	for(int i=1;i<n-1;i+=2) {
-		if (a[i]==100) {
-			ans+=a[i+1];
-		} else if(a[i]==200) {
-			ans-=a[i+1];
-		} else if(a[i]==300) {
-			ans*=a[i+1];
-		} else if(a[i]==400) {
-			ans/=a[i+1];
-		}
-	}
-	cout<<ans<<"\n";
-}
-
 int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	solve();	
-	return 0;
+    int i=0;
+    int sum=0;
+    int a[1000];
+    int t;
+    cin >> t;
+    for (int i=0;i<t;i++)
+    {
+        cin>>a[i];
+    }
+    for(int j=0; j<t;j++)
+    {
+            if(a[j] == 100){
+                a[j] = a[j-1] + a[j+1];
+            }
+            if(a[j] == 200){
+                a[j] =  a[j-1] - a[j+1];
+            }
+            if(a[j] == 300){
+                a[j] = a[j-1] * a[j+1];
+            }
+            if(a[j] == 400){
+                a[j] =  a[j]-1 / a[j]+1;
+            }
+    }
+    for(int i=0;i<t;i++) {
+        if (a[i] % 2 == 0) {
+            sum+=a[i];
+        }
+    }
+    cout << sum;
+    return 0;
 }
